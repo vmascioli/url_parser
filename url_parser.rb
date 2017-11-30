08c3b534b061c51e8f42508a9613e1cc37bb5552
Victoria Mascioli <vmascioli@email.lynn.edu>
2017-11-29 19:35:04 -0500

    first commit

--- url_parser.rb
+++ url_parser.rb
@@ -1,5 +1,19 @@
 class UrlParser
 	def initialize(url)
-    
+		@url = url
+    end
+    def scheme
+    	@url.split("://").first	
+    end
+    def domain 
+    	@url.split("//")[1].split(":")[0]
+    end
+    def port
+    	 @url.split("//")[1].split("/")[0].split(":")[1]
     end
  end
+
+
+@new_url = UrlParser.new("http://www.google.com:60/search?q=cat&name=Tim#img=FunnyCat")
+
+p @new_url.domain
